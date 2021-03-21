b5c6f5dd1d25417db3cca650dd2b90e93f4f7941
Manoj-Paramsetti <paramsetti.manoj@gmail.com>
2021-03-21 06:57:11 +0530

    added localStorage

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-var list = []
-var string = ""
-var initial = ''
+var list=[]
 var i=0
+var initial = ""
 function add(){
     if(document.getElementById('input').value!=""){
         list.push(document.getElementById('input').value)
+        document.getElementById('input').value = ""
+        i = list.length-1
+        console.log(list)
         string="<div class = 'card'>"+list[i]+"<button onclick='remove("+i+")'>delete</button></div>";
         document.getElementById('cards').innerHTML = document.getElementById('cards').innerHTML+(string)
-        i = i+1
     }
+    localStorage.list= list
 }
 function remove(n){
     document.getElementById('cards').innerHTML = initial
     console.log(list.length)
-    list.splice(n,1) 
-    console.log(list.length)
+    list.splice(n,1)
+    localStorage.setItem('list',list) 
     for (i=0; i < list.length; i++){
         console.log(list)
         string="<div class = 'card'>"+list[i]+"<button onclick='remove("+i+")'>delete</button></div>";
         document.getElementById('cards').innerHTML = document.getElementById('cards').innerHTML+(string)
     }
+    localStorage.list= list
 }
+function getData(){
+    console.log(list)
+    if(localStorage.list){
+        console.log(localStorage.list.length)
+        list = localStorage.list.split(",")
+        for (i=0; i < list.length; i++){
+            console.log(list)
+            string="<div class = 'card'>"+list[i]+"<button onclick='remove("+i+")'>delete</button></div>";
+            document.getElementById('cards').innerHTML = document.getElementById('cards').innerHTML+(string)
+        }
+    }
+    else{
+        localStorage.list= []
+        list = []
+    }
+}
+window.addEventListener("keydown", (a)=>{
+  if (a.key=="Enter") {
+    if(document.getElementById('input').value!=""){
+        list.push(document.getElementById('input').value)
+        document.getElementById('input').value = ""
+        i = list.length-1
+        console.log(list)
+        string="<div class = 'card'>"+list[i]+"<button onclick='remove("+i+")'>delete</button></div>";
+        document.getElementById('cards').innerHTML = document.getElementById('cards').innerHTML+(string)
+    }
+    localStorage.list= list    
+  }  
+})
